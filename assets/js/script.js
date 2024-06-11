d8cdd5fd6cdf80d28c2af536738cb4137d53e244
IbraKristo <lomeseth1@gmail.com>
2024-06-11 23:42:45 +0300

    add details

--- assets/js/script.js
+++ assets/js/script.js
@@ -138,29 +138,46 @@ favButton.addEventListener("click", (e) => {
 })
 
 document.addEventListener("DOMContentLoaded", async () => {
-    let data = await fetch(`${baseURL}/api/courses`);
+    const currentUrl = window.location.href;
+    const url = new URL(currentUrl);
+    const queryParams = url.searchParams;
+    const id = queryParams.get("id");
+    if (!id) {
+        let data = await fetch(`${baseURL}/api/courses`);
+        data = await data.json();
+        const div = document.querySelector(".cards-body");
+        div.innerHTML = "";
+        div.classList.remove("spinner_container");
+        await createCards(data);
+
+        let searchInput = document.querySelector(".search-input");
+        searchInput.addEventListener("input", async () => {
+            if (typeof currentTimeOut === "number") {
+                clearTimeout(currentTimeOut);
+            }
+
+            currentTimeOut = setTimeout(async () => {
+                search();
+            }, 500)
+        })
+        let cardsBody = document.querySelector(".cards-body");
+        cardsBody.addEventListener("click", (e) => {
+            const item = e.target.closest(".card");
+            if (item && cardsBody.contains(item)) {
+                const id = item.getAttribute("data-key");
+                window.location.href = `./details.html?id=${id}`
+            }
+
+        })
+        return;
+    }
+
+    let data = await fetch(`${baseURL}/api/courses/${id}`);
     data = await data.json();
-    await createCards(data);
-    let searchInput = document.querySelector(".search-input");
-    searchInput.addEventListener("input", async () => {
-        if (typeof currentTimeOut === "number") {
-            clearTimeout(currentTimeOut);
-        }
-
-        currentTimeOut = setTimeout(async () => {
-            search();
-        }, 500)
-    })
-    let cardsBody = document.querySelector(".cards-body");
-    console.log(cardsBody)
-    cardsBody.addEventListener("click", (e) => {
-        const item = e.target.closest(".card");
-        if (item && cardsBody.contains(item)) {
-            const id = item.getAttribute("data-key");
-            window.location.href = `./details.html?id=${id}`
-        }
-
-    })
+    const div = document.querySelector(".cards-body");
+    div.innerHTML = "";
+    div.classList.remove("spinner_container");
+    await createDetails(data);
 })
 
 document.addEventListener("keydown", async (e) => {
@@ -190,10 +207,6 @@ async function search() {
 function createCards(data) {
     const cardsTitle = document.querySelector(".cards-head");
     cardsTitle.textContent = `"${data ? data.length : 0}" Web Topics Found`
-
-    const div = document.querySelector(".cards-body");
-    div.innerHTML = "";
-    div.classList.remove("spinner_container");
     for (let i = 0; i < data.length; i++) {
         createCard(data[i]);
     }
@@ -267,3 +280,127 @@ function createCard(data) {
     document.querySelector(".cards-body").appendChild(card);
 }
 
+function createDetails(data) {
+    console.log(data);
+
+    const detailsHeader = document.createElement('div');
+    detailsHeader.className = 'details_header flex-row justify-center';
+
+    const detailsContainer = document.createElement('div');
+    detailsContainer.className = 'details_container flex-row position-relative';
+
+    const detailsDetails = document.createElement('div');
+    detailsDetails.className = 'details_details flex-col padding-3 gap-4';
+
+    const detailsDetailsHeader = document.createElement('div');
+    detailsDetailsHeader.className = 'details_details_header';
+
+    const category = document.createElement('h5');
+    category.className = 'category';
+    category.textContent = `${data.category}`;
+
+    const topic = document.createElement('h2');
+    topic.className = 'topic';
+    topic.textContent = `${data.topic}`;
+
+    const ratings = document.createElement('div');
+    ratings.className = 'ratings';
+
+    const stars = [];
+
+    const ratingsNum = Math.round(data.rating * 2) / 2;
+    for (let i = 0; i < ratingsNum; i++) {
+        stars.push({ src: './assets/img/star.svg', alt: 'checked_star' });
+    }
+    if (ratingsNum - Math.floor(ratingsNum) !== 0) {
+        stars.push({ src: './assets/img/star-half.svg', alt: 'half_star' });
+    }
+    for (let i = 0; i < 5 - Math.ceil(ratingsNum); i++) {
+        stars.push({ src: './assets/img/star-outline.svg', alt: 'unchecked_star' });
+    }
+
+    stars.forEach(starData => {
+        const star = document.createElement('img');
+        star.className = 'star';
+        star.src = starData.src;
+        star.alt = starData.alt;
+        ratings.appendChild(star);
+    });
+
+    detailsDetailsHeader.append(category, topic, ratings);
+
+    const description = document.createElement('div');
+    description.className = 'description';
+    description.textContent = `${data.description}`;
+
+    detailsDetails.append(detailsDetailsHeader, description);
+
+    const detailsImgContainer = document.createElement('div');
+    detailsImgContainer.className = 'details_img_container flex-grow-1 position-absolute right_300 top-25';
+
+    const detailsImg = document.createElement('img');
+    detailsImg.className = 'details_img';
+    detailsImg.src = `./assets/img/${data.image}`;
+    detailsImg.alt = `${data.topic}`;
+
+    const detailsImgBody = document.createElement('div');
+    detailsImgBody.className = 'details_img_body font-weight-2 font-small padding-1';
+
+    const detailsImgText = document.createElement('p');
+    detailsImgText.className = 'margin-bottom-1 font-weight-3';
+    detailsImgText.innerHTML = 'HTML by <span class="details_author">Sarah Smith</span>';
+
+    const addToFavContainer = document.createElement('div');
+    addToFavContainer.className = 'add_to_fav_container padding-1 flex-col align-center';
+
+    const favText = document.createElement('p');
+    favText.className = 'align-self-flex-start';
+    favText.textContent = 'interested about this topic?';
+
+    const favButton = document.createElement('button');
+    favButton.className = 'details_fav_buttons flex-row align-center gap-2';
+    favButton.innerHTML = '<span>Add to Favorites</span><img class="details_fav_icon" src="./assets/img/favourites.svg" alt="fav">';
+
+    const unlimitedCredits = document.createElement('span');
+    unlimitedCredits.className = 'details_unlimited';
+    unlimitedCredits.textContent = 'Unlimited credits';
+
+    addToFavContainer.append(favText, favButton, unlimitedCredits);
+    detailsImgBody.append(detailsImgText, addToFavContainer);
+    detailsImgContainer.append(detailsImg, detailsImgBody);
+
+    detailsContainer.append(detailsDetails, detailsImgContainer);
+    detailsHeader.appendChild(detailsContainer);
+
+    const detailsTopics = document.createElement('div');
+    detailsTopics.className = 'details_topics flex-row justify-center';
+
+    const detailsTopicsContainer = document.createElement('div');
+    detailsTopicsContainer.className = 'details_topics_container padding-3';
+
+    const detailsTopicsBody = document.createElement('div');
+    detailsTopicsBody.className = 'details_topics_body';
+
+    const detailsTopicsHeader = document.createElement('div');
+    detailsTopicsHeader.className = 'details_topics_header border-bottom-1 padding-2 font-weight-3 font-large';
+    detailsTopicsHeader.textContent = `${data.topic} Sub Topics`;
+
+    const subTopicsContainer = document.createElement('div');
+    subTopicsContainer.className = 'details_topics_body';
+
+    for (let i = 0; i < data.subtopics.length; i++) {
+        const subTopic = document.createElement('div');
+        subTopic.className = 'details_sub_topics border-bottom-1 padding-1 flex-row gap-2';
+        subTopic.innerHTML = `<img class="check" src="./assets/img/checkmark-circle.svg" alt="checked"><span>${data.subtopics[i]}</span>`;
+        subTopicsContainer.appendChild(subTopic);
+    }
+
+    detailsTopicsBody.append(detailsTopicsHeader, subTopicsContainer);
+    detailsTopicsContainer.appendChild(detailsTopicsBody);
+    detailsTopics.appendChild(detailsTopicsContainer);
+
+    const mainContainer = document.getElementsByTagName('main');
+    mainContainer[0].append(detailsHeader, detailsTopics);
+
+
+}
